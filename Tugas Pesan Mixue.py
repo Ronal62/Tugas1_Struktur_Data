0f5a9a2b4a4b52ee8025de1a0d1a5c90e214a2c1
refanydwi <146598703+refanydwilestari@users.noreply.github.com>
2024-03-13 18:49:54 +0700

    Update Tugas Pesan Mixue.py

--- Tugas Pesan Mixue.py
+++ Tugas Pesan Mixue.py
@@ -2,7 +2,7 @@
 #Kelompok 3
 #Nama : Ronald Budi Abdul Wahid   #NIM :23091397142
 #Nama : Muhammad Ibnu Nadhif      #NIM :23091397161
-#Nama :                           #NIM :
+#Nama : Refany Dwi Lestari        #NIM :23091397170
 
 class ItemMenu:
     def __init__(self, nama, harga):
