4e7c6c64f564c777c8adbd6e68d28561a81281e8
Ronald Budi Abdul Wahid <106757452+Ronal62@users.noreply.github.com>
2024-03-14 23:22:31 +0700

    Update Tugas Pesan Mixue.py
    Edit Nama Kelompok

--- Tugas Pesan Mixue.py
+++ Tugas Pesan Mixue.py
@@ -1,5 +1,5 @@
 #Struktur Data
-#Kelompok 2
+#Kelompok 3
 #Nama : Ronald Budi Abdul Wahid   #NIM :23091397142
 #Nama : Muhammad Ibnu Nadhif      #NIM :23091397161
 #Nama : Refany Dwi Lestari        #NIM :23091397170
